34d23a2b04e48fe5af4daea8261d0455ce9db01b
Greg <gregory.marshall999@gmail.com>
2024-12-17 14:46:51 +0100

    wobble animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,13 +26,42 @@ const triggerToast = () => {
 	justify-content: center;
 }
 
-.toast-enter-from, .toast-leave-to {
+.toast-leave-to {
 	opacity: 0;
 	transform: translateY(-60px);
 }
-
-.toast-enter-active, .toast-leave-active {
+.toast-leave-active {
 	transition: all 0.3s ease;
+}
+
+.toast-enter-active {
+	animation: wobble 0.5s ease;
+}
+
+@keyframes wobble {
+	0% {
+		opacity: 0;
+		transform: translateY(-60px);
+	}
+	50% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+	60% {
+		transform: translateX(8px);
+	}
+	70% {
+		transform: translateX(-8px);
+	}
+	80% {
+		transform: translateX(4px);
+	}
+	90% {
+		transform: translateX(-4px);
+	}
+	100% {
+		transform: translateX(0);
+	}
 }
 
 </style>
